8917dda413cd3ac1c2d7fca5caa5e5e56d634b7e
jacob <jacobhalleron@protonmail.com>
2021-01-19 17:33:10 +0000

    anfa

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-var http = require("http")
-var fs = require("fs")
-var path = require("path")
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
+const express = require("express"); const app = express()
 
-http.createServer(function (req, res) {
-	if (req.url == "/") {
-		res.writeHead(200, {"Content-Type": "text/html"})
-		var fileStream = fs.createReadStream("./index.html")
-		fileStream.pipe(res)
-	}
-	else if (req.url.match("\.css$")) {
-		res.writeHead(200, {"Content-Type": "text/css"})
-		var fileStream = fs.createReadStream(path.join("." + req.url))
-		fileStream.pipe(res)
-	}
-	else if (req.url.match("\.js$")) {
-		res.writeHead(200, {"Content-Type": "text/javascript"})
-		var fileStream = fs.createReadStream(path.join("." + req.url))
-		fileStream.pipe(res)
-	}
-	else {
-		res.writeHead(404, {"Content-Type": "text/html"})
-		var fileStream = fs.createReadStream("./404.html")
-		fileStream.pipe(res)
-	}
-}).listen(8080)
+app.use(express.static("public"))
+
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.listen(8080)
